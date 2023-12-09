8af627ddf98b80f35e477844ae425f2d8d50c6e4
M Roy <mroy.in21@gmail.com>
2023-12-09 22:23:44 +0530

    Implement routing and Clerk authentication platform

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AskQuestion = () => {
+  return <div>ask-question page</div>;
+};
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className=" flex flex-col items-center justify-between gap-5 p-24">
-      <h1 className=" flex-center h1-bold">Hello Next.js 14!</h1>
-      <h1 className=" flex-center h2-bold">Hello Next.js 14!</h1>
-      <h1 className=" flex-center h3-bold">Hello Next.js 14!</h1>
-    </main>
-  );
-}
