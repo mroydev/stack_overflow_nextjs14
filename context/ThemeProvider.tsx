2563a1f7cb030f708548c9bfcbd65c35aa4ba1e9
M Roy <mroy.in21@gmail.com>
2023-12-10 19:25:19 +0530

    Creating a Global Theme Context for DevFlow

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,49 @@
+'use client';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState('light');
+
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      setMode('dark');
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.add('dark');
+    } else {
+      setMode('light');
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
+    }
+  };
+
+  useEffect(() => {
+    handleThemeChange();
+  }, [0]); // [0]: Runs only once on mount. [mode] : Runs whenever mode changes.
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+
+  return context;
+}
