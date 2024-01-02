5a1eaa922417b054484beea080aaea28c2ebc97a
M Roy <mroy.in21@gmail.com>
2024-01-02 23:24:51 +0530

    Implement Webhooks and User Actions

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,22 +1,33 @@
 import Question from '@/components/forms/Question';
 import { getUserById } from '@/lib/actions/user.action';
+// import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
-const AskQuestion = async () => {
+const Page = async () => {
   // const { userId } = auth();
-
-  const userId = 'clerk123';
+  const userId = '12345';
   if (!userId) redirect('/sign-in');
 
   const mongoUser = await getUserById({ userId });
 
-  console.log(mongoUser);
+  if (!mongoUser) {
+    return (
+      <h1 className="text-center text-2xl font-semibold text-slate-900">
+        User not found!
+      </h1>
+    );
+  }
+
   return (
     <div>
-      <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
     </div>
   );
 };
 
-export default AskQuestion;
+export default Page;
