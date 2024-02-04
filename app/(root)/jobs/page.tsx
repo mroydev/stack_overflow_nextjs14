4a14987bd6e27f0a776f8f9c6782d203f001f873
M Roy <mroy.in21@gmail.com>
2024-02-04 12:31:04 +0530

    jobs page

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Jobs</h1>
+      <p className=" text-dark100_light900">Jobs page content</p>
+    </div>
+  );
+};
+
+export default Page;
